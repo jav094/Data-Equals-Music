ecf86d92e72a5e9e6fc17108874377d3f9d92010
Juan Valera <jav094@gmail.com>
2016-08-17 18:40:33 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# import ALL THE THINGS
+import pandas as pd
+import numpy as np
+from sklearn.cross_validation import train_test_split
+from sklearn.cross_validation import cross_val_score
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+from textblob import TextBlob, Word
+import seaborn as sea
+import spotipy
+
+
+# Hit the Spotify API for a specific track's audio featuers (e.g. valence, key)
+# def getAudioFeatures(url):
+#     return " "
+
+# Create a DataFrame
+file = "data/global-spotify-top200-8-17.csv"
+# sp = pd.read_csv(file)
+
+spotify = spotipy.Spotify()
+
+name = "Justin Timberlake"
+results = spotify.search(q='artist:' + name, type='artist')
+print results
+
+# print sp.head()
