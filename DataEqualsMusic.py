96221c6934db1e7b858f8a0e8384d5313bd5cb14
Juan Valera <jav094@gmail.com>
2016-08-29 18:34:59 -0700

    Did a buncha stuff. Moved analysis into python notebook, removed analysis.py, tweaked .gitignore, and fixed a bug in my API.

--- DataEqualsMusic.py
+++ DataEqualsMusic.py
@@ -6,14 +6,12 @@
     track information from Spotify!
 """
 
-
 import pandas as pd
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-import pprint
-pp = pprint.PrettyPrinter(depth=4)
 
 class SpotiAPI(object):
+
     def __init__(self):
         file = "data/global-spotify-top200-8-18.csv"
         self.df = pd.read_csv(file)
@@ -31,15 +29,10 @@ class SpotiAPI(object):
         # Get track id, audio features, track info, and other features into DataFrame
         self.add_track_id_to_frame()
         self.add_audio_features_to_frame(sp_client)
-        # self.add_track_info_to_frame(sp_client)
         # self.add_lyrics_to_frame(sp_client)
 
         # Do this last
         self.clean_frame()
-
-    # Override "represent" for this class so that it's' formatted nicely when we try to print it.
-    def __repr__(self):
-        return str(pp.pprint(self.df))
 
     # Adds TrackID feature, sliced from URL
     def add_track_id_to_frame(self):
@@ -58,38 +51,34 @@ class SpotiAPI(object):
     # Fetches audio features for specific tracks and adds them to the df.
     def add_audio_features_to_frame(self, sp_client):
         for index, row in self.df.iterrows():
-            # Get audio features for the current track
+            # Hit the Spotify API for the current track's audio features.
             # Note: we need [0] to actually get IN the dictionary, otherwise this returns a list containing the dict.
             track_audio_features = sp_client.audio_features(tracks=[row.TrackID])[0]
 
-            # Iterate through the audio features and add each as a new feature column
+            # Iterate through the audio features and add each as a new feature column.
             for key, value in track_audio_features.items():
                 self.df.loc[index,key] = value
-
-    # Fetches track info for specific track and adds them to the df.
-    def add_track_info_to_frame(self, sp_client):
-        raise NotImplementedError("Not implemented")
 
     # Fetches track lyrics for specific track and adds them to the df.
     def add_lyrics_to_frame(self, sp_client):
         raise NotImplementedError("Not implemented")
 
-    # Drops unnneeded features (e.g. Streams)
+    # Drops unnneeded features.
     def clean_frame(self):
-        cols_to_drop = ["Streams", "URL", "analysis_url", "track_href","type"]
-        self.df.drop(cols_to_drop,inplace=True,axis=1)  
+        cols_to_drop = ["Streams", "URL", "analysis_url", "track_href", "type", "uri", "id", "Track Name", "Artist", "TrackID"]
+        self.df.drop(cols_to_drop, inplace=True, axis=1)  
 
-    # Export dataframe to a .csv just in case the API stops liking us.
+    # Export dataframe to a .csv (which is what we do our analysis on).
     def save_to_csv(self, file):
         self.df.to_csv(path_or_buf=file)
         print("Exported dataframe to file: %s" % (file))
     
-    # Return dataframe for use by other classes.
+    # Return the dataframe (for use by external classes).
     def get_dataframe(self):
         return self.df
 
 
-# If this file is run on its own, it'll export the dataframe to a csv so we can analyze the data offline.
+# If this .py file is run on its own, it'll export the dataframe to a csv so we can analyze the data offline.
 if __name__ == '__main__':
     api = SpotiAPI()
     api.save_to_csv("data/modified_spotify_top_200.csv")
